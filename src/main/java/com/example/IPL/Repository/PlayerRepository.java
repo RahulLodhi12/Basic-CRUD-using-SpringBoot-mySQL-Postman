3ed2ab6c708fa443a40fe27212d22d56ebc0c868
Rahul Lodhi <rahullodhimts@gmail.com>
2025-04-01 21:37:12 +0530

    first commit

--- src/main/java/com/example/IPL/Repository/PlayerRepository.java
+++ src/main/java/com/example/IPL/Repository/PlayerRepository.java
@@ -2,7 +2,9 @@ package com.example.IPL.Repository;
 
 import com.example.IPL.Entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player,Long> {
 
 }
